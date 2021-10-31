d609571115225e8044fd9f4c99c354b9e372f3e4
inaciojr48 <54165291+inaciojr48@users.noreply.github.com>
2021-10-31 11:22:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-const http = require('http')
-const express = require('express')
-
+const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
+    console.log('first middleware')
+    next()
+});
 
-    console.log('Loading middleware')
-    next(); //alows to work with another middle ware
-})
+app.use('/users', (req, res, next) => {
+    console.log('users middleware')
+    res.send('<h1>Congrats you follow the way up</h1>')
+});
 
-app.use((req, res, next) => {
-
-    console.log('Loading next middleware')
-    res.send("<h1>Hello world from express</h1>")
-  
-})
-const server = http.createServer(app);
-server.listen(3000);
-
+app.listen(3000)
