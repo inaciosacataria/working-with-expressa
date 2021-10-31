dc29cfd3fd8ddd1cbd0efd08fb05d27e24cae831
inaciojr48 <54165291+inaciojr48@users.noreply.github.com>
2021-10-31 20:37:29 +0200

    styles added

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 
 router.get('/add-product', (req, res, next) => {
-    const VIEWS_URL= path.join(__dirname, '../','views','add-product.html');
+    const VIEWS_URL= path.join(rootDir,'views','add-product.html');
     res.sendFile(VIEWS_URL);
     
 });
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
+const rootDir = require('../util/path');
 
 const router= express.Router();
 
 router.get('/', (req, res, next) => {
-    const VIEW_URL = path.join(__dirname,'../', 'views', 'shop.html');
+    const VIEW_URL = path.join(rootDir, 'views', 'shop.html');
     res.sendFile(VIEW_URL)
 });
 
