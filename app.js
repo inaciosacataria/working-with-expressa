5a86887a5f162391e388469782d22b3f1dbf7890
inaciojr48 <54165291+inaciojr48@users.noreply.github.com>
2021-11-01 06:58:15 +0200

    css added

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 const adminRoutes = require('./routes/admin');
 const shopRoutes  = require('./routes/shop');
+
+
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
