3fd2de85d1eef4ccd5e22268015dd23f59210548
inaciojr48 <54165291+inaciojr48@users.noreply.github.com>
2021-11-03 05:15:53 +0200

    update

---
+++ imgurl.js
@@ -0,0 +1,5 @@
+const urls = [
+    "https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_465,q_auto,w_310/v1631738383/amc-cdn/production/2/movies/66900/66945/PosterDynamic/128342.jpg",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mv5bymm0nzriodetmtizns00mtgwlwfjzdqtnjuxzji0mddjzwq4xkeyxkfqcgdeqxvymtmxodk2otuat-v1-1580764836.jpg?crop=1xw:1xh;center,top&resize=480:*",
+
+]
