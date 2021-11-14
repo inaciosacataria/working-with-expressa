2f269fe1c6d78281eba79318c7051ade7789bb5a
inaciojr48 <54165291+inaciojr48@users.noreply.github.com>
2021-11-14 10:14:13 +0200

    Update the pug files extending an deafult layout

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ router.get('/', (req, res, next) => {
    // const VIEW_URL = path.join(rootDir, 'views', 'shop.html');
   //  res.sendFile(VIEW_URL)
     const products = adminData.product;
-    res.render('shop',{prods:products, docTitle:'Shop'})
+    res.render('shop',{prods:products, docTitle:'Shop',path:'/'})
 });
 
 module.exports = router
