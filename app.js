9637473c27271a0dac1e3895758e6927e77751b5
inaciojr48 <54165291+inaciojr48@users.noreply.github.com>
2021-11-20 14:23:06 +0200

    controllers added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
+const errorPages = require('./controllers/errorPage')
 const app = express();
 
 
@@ -20,8 +20,6 @@ app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404',{docTitle:'Ejs is good'});
-});
+app.use(errorPages);
 
 app.listen(3000);
